c3240626be2563268d6c3b659b466a38b0433f3a
lijia <lijia@huajiao.tv>
2019-04-23 20:56:30 +0800

    init project

---
+++ JenTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JenTest
+//
+//  Created by lijia on 2019/4/23.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
