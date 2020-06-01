830bf8436a3759e56c3244302ccbb81bed452048
lijia <lijia@huajiao.tv>
2020-06-01 19:08:00 +0800

    stream test

--- JenUnitTests/RACSignalTests.m
+++ JenUnitTests/RACSignalTests.m
@@ -43,6 +43,31 @@
 - (void)testReturn {
     
 }
+
+- (void)testZip {
+    
+    RACSignal *s = [RACSignal zip:@[[RACSignal return:@1],
+                                    [RACSignal return:@2],
+                                    [RACSignal return:@3]]];
+    
+    [s subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@", x);
+    }];
+}
+
+- (void)testZipReduce {
+    
+    RACSignal *s = [RACSignal zip:@[[RACSignal return:@1],
+                                    [RACSignal return:@2],
+                                    [RACSignal return:@3]] reduce:^ NSString * (id x, id y, id z) {
+        return [NSString stringWithFormat:@"%@ %@ %@", x, y, z];
+    }];
+    
+    [s subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@", x);
+    }];
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
--- JenUnitTests/RACStreamTests.m
+++ JenUnitTests/RACStreamTests.m
@@ -9,19 +9,50 @@
 #import <XCTest/XCTest.h>
 #import "Frameworks.h"
 
+typedef RACStream *(^streamWithValues)(NSArray *);
 @interface RACStreamTests : XCTestCase
-
+@property (nonatomic, copy) streamWithValues  sv;
 @end
 
 @implementation RACStreamTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    Class streamClass = RACSignal.class;
+    self.sv = [^(NSArray *values) {
+        RACStream *stream = [streamClass empty];
+
+        for (id value in values) {
+            stream = [stream concat:[streamClass return:value]];
+        }
+
+        return stream;
+    } copy];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
+- (void)testReduceEach {
+    RACSignal *signal = self.sv(@[
+        RACTuplePack(@"foo", @"bar"),
+        RACTuplePack(@"buzz", @"baz"),
+        RACTuplePack(@"", @"_")
+    ]);
+    
+    RACSignal *s = [signal reduceEach:^(NSString *a, NSString *b) {
+        return [a stringByAppendingString:b];
+    }];
+    [s subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@", x);
+    }];
+}
+
+- (void)testStart {
+    RACSignal *s = [[RACSignal empty] startWith:@1];
+    [s subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@", x);
+    }];
+}
 
 @end
